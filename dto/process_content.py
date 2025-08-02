5848f26edb8be23569ab5fb0c5eb64e587f5abf3
daehyeong2 <baconbacon1231@gmail.com>
2025-08-03 01:43:17 +0900

    feat: instruction대로 메시지 가공하는 방식으로 변경

--- dto/process_content.py
+++ dto/process_content.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 class ProcessContentRequest(BaseModel):
     text: str
     target: str
+    instruction: str
 
 class ProcessContentResponse(BaseModel):
     processed_content: str
